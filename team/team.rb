c81dc7be146fbb89a9c08474fc37ba5ff332e146
Faisal <faisal_a@live.co.uk>
2017-05-29 17:44:31 +0100

    Further through Part B of classes h/work

--- team/team.rb
+++ team/team.rb
@@ -1,5 +1,7 @@
 class Team
 
+attr_reader :team_name, :players, :coach_name
+attr_writer :coach_name
 
 def initialize(team_name, players, coach_name) 
   @team_name = team_name
@@ -7,24 +9,33 @@ def initialize(team_name, players, coach_name)
   @coach_name = coach_name
 end  
 
-def team_name
-  return @team_name
+# def team_name
+#   return @team_name
+# end  
+
+# def players_names 
+#   return @players
+# end   
+
+# def coach_name
+#   return @coach_name
+# end
+
+# def set_coach_name(new_name)
+#   @coach_name = new_name
+# end
+
+def add_new_player(new_name)
+  return @players.push(new_name)
+end
+
+def finding_player(name)
+  for player in @players
+    if player == name
+      return name
+    end
+  end  
 end  
-
-def players_names 
-  return @players
-end   
-
-def coach_name
-  return @coach_name
-end
-
-def set_coach_name(new_name)
-  @coach_name = new_name
-end
-
-
-
 
 
 
--- team/team_spec/team_spec.rb
+++ team/team_spec/team_spec.rb
@@ -12,7 +12,7 @@ def test_team_name
 end
 
 def test_player_name
-  assert_equal(["James", "Glenn"], @team.players_names)
+  assert_equal(["James", "Glenn"], @team.players)
 end  
 
 def test_coach_name
@@ -20,12 +20,18 @@ def test_coach_name
 end
 
 def test_set_coach_name
-  assert_equal("Sandy", @team.set_coach_name("Sandy"))
+  @team.coach_name = "Sandy"
+  assert_equal("Sandy", @team.coach_name)
 end 
 
+def test_new_player_added
+    assert_equal(["James", "Glenn", "Damien"], @team.add_new_player("Damien"))
+end
 
+def test_player_found
+  assert_equal("Glenn", @team.finding_player("Glenn"))
+end  
 
-# Create a setter method to allow the coach's name to be updated.
 
 
 
